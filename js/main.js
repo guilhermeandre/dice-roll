44f62f3005c2c234971b10db44e21c3c8bce26c1
guilhermeandre <guilherme1@outlook.com>
2020-04-08 16:56:36 -0400

    Initial development. Classes dice-number created. Proposed solution added to JS file.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,29 @@
 let roll = Math.floor(Math.random() * 6) + 1
 
 console.log(roll);  // 1, 2, 3, 4, 5, or 6
+
+
+
+//1. Idetify what initial face dice is: you need to check the face so you can change it ramdomly. 
+
+//2. Change the dice image ramdomly: imagine that each face of the dice is an image, so, everytime you want it to change, you change the image. 
+
+//3. 
+
+
+
+
+
+
+
+
+
+// let theH1Element = document.querySelector('h1');
+
+
+// theH1Element.innerHTML = `Cansado dessa <em>porra</em>.`;
+// theH1Element.style.color = 'limegreen';
+// theH1Element.className.add(`flip`);
+// theH1Element.className.add(`bg`);
+// theH1Element.setAttribute(`title`, `Hello World`);
+
